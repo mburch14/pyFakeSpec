e71afd017dbc60fbf3a5814ed3d1b78f9d78de28
Michael Burch <michaelburch797@gmail.com>
2026-07-16 16:30:43 -0700

    Initial commit

---
+++ MissionClasses.py
@@ -0,0 +1,239 @@
+import math
+from matplotlib.pylab import det
+import numpy as np
+from astropy.io import fits
+import xraydb
+from abc import ABC, abstractmethod
+import subprocess
+
+class Mission:
+
+    def __init__(self, name, energymin, energymax):
+        self.name = name
+
+        #Energy range in keV
+        self.energymin = energymin
+        self.energymax = energymax
+
+
+class Orbit:
+
+    def __init__(self, altitude, inclination=0):
+        self.earthradius = 6378.137  # km
+        self.altitude = altitude #km
+        self.inclination = inclination #degrees
+
+        self.theta = np.arcsin(self.earthradius / (self.earthradius + altitude)) #radians, angle between the zenith and the horizon.
+
+        #The fraction of the sky blocked by the Earth, assuming a circular orbit and a spherical Earth.
+        self.earth_blocking_fraction = 0.5 * (1 - np.cos(self.theta))
+
+    def __str__(self):
+        return (f"{self.altitude} km circular orbit\n" f"Inclination: {self.inclination}°\n")
+
+
+class geometry:
+
+    defaults = {
+    "detl": 90, #in cm, length of the detector
+    "detw": 60, #in cm, width of the detector
+    "dett": 2, #in mm, thickness of the detector
+    "deth": 5.0, #in cm, height from the bottom of the cubesat to the detector
+    "imagTransmission": 0.5, #Fraction of the photons that make it through the optics and onto the detector.
+    "length": 100.0, #in cm, length of the cubesat
+    "width": 100.0, #in cm, width of the cubesat
+    "height": 100.0,} #in cm, height of the cubesat
+
+    def __init__(self, config=None):
+        config = config or {} #either use the provided config or an empty dict if none is provided
+        settings = self.defaults.copy()
+        settings.update(config) #add any inputted config values to the defaults
+
+        #dimension of the detector in cm
+        self.detl = settings["detl"]
+        self.detw = settings["detw"]
+        self.detthickness = settings["dett"] #thickness of the detector
+        self.deth = settings["deth"] #from the bottom of the cubesat to the detector
+
+        #represented as a decimal between 0 and 1, where 1 is 100% transmission and 0 is 0% transmission, so far.
+        self.imagTransmission = settings["imagTransmission"]
+
+        #dimension of the cubesat in cm
+        self.l = settings["length"]
+        self.w = settings["width"]
+        self.h = settings["height"]
+
+        #wfov is the field of view in the width direction; lfov is the field of view in the length direction
+        self.wfov = 2*math.atan((self.w -self.detw) / (2*(self.h-self.deth))) #in radians
+        self.lfov = 2*math.atan((self.l -self.detl) / (2*(self.h-self.deth))) #in radians
+
+        #total field of view in steradians, calculated using the formula for the solid angle of a rectangle
+        self.fov_sr = 4 * np.arcsin(np.sin(self.wfov / 2) * np.sin(self.lfov / 2))
+
+        #collecting area in cm^2; assuming that the photons are othogonal to the detector. if not safe assumption, multiply by cos(theta).
+        self.collecting_area = self.detl * self.detw  * self.imagTransmission
+
+    def __str__(self):
+        return f"This is a {(self.l* self.w* self.h)/1000}U cubesat with a {round(self.wfov, 2)} by {round(self.lfov, 2)} radian field of view and {self.imagTransmission*100}% transmission."
+
+
+class BackgroundModel:
+    def __init__(self, detector):
+        self.detector = detector
+
+
+class CXB(BackgroundModel):
+    def photonIntensity(self, energy, fov_sr):
+        #Cosmic X-ray background spectrum from Gruber et al. 1999, ApJ, 520, 124
+        #In units of photons/cm2/s/sr/keV
+        
+        results = []
+        for e in energy:
+            if 3 < e < 60:
+                test = 7.877 * e**(-0.29) * math.exp(-e / 41.13)
+            elif e >= 60:
+                test = (0.0259 * (e / 60)**(-5.5) + 0.504 * (e / 60)**(-1.58) + 0.0288 * (e / 60)**(-1.05))
+            else:
+                results.append(0)
+                continue
+
+            results.append(fov_sr * test / e)
+        return results
+
+    def gen_spectrum_table(self, output="cxb_background.dat"):
+        energies = self.detector.energy #Gives energy bin midpoints in keV
+        energy_lo = self.detector.energy_low #Gives energy bin lower bounds in keV
+        energy_hi = self.detector.energy_high #Gives energy bin upper bounds in keV
+
+        #flux in units of photons/cm2/s/keV
+        fluxes = np.array(self.photonIntensity(energies, self.detector.geos.fov_sr))
+        table = np.column_stack((energy_lo, energy_hi, fluxes))
+        np.savetxt(output, table, fmt="%.6f %.6f %.8e", comments="")
+        return output
+
+
+class Albedo(BackgroundModel):
+    def photonIntensity(self, energy, fov_sr):
+        #Albedo spectrum from Ajello et al. 2008, ApJ, 689, 666
+        #In units of photons/cm2/s/sr/keV
+        results = []
+        e_B = 33.7  # Break energy in keV
+        Gamma1 = -5
+        Gamma2 = 1.72
+        const = 1.48e-2 #it is 1.48e-2
+        for e in energy:
+            results.append(fov_sr * const / ((e / e_B)**(Gamma1) + (e / e_B)**(Gamma2)))
+
+        return results
+
+
+class ChargedParticles(BackgroundModel):
+    def photonIntensity(self, energy, fov_sr):
+        return 0
+
+
+class detector(ABC):
+    def __init__(self, geography, orbit, mission):
+        self.geos = geography
+        self.orbs = orbit
+        self.missions = mission
+        self.energy_edges = np.linspace(self.missions.energymin, self.missions.energymax, self.missions.energymax-self.missions.energymin+1)   #1 keV bins
+        self.energy_low = self.energy_edges[:-1] #zeroth element to second-to-last element
+        self.energy_high = self.energy_edges[1:] #first element to last element
+        self.energy = 0.5*(self.energy_low + self.energy_high) #Midpoint of each energy bin.
+        self.res = 0
+
+    @abstractmethod
+    def effective_area(self, energy):
+        pass #All effective area calculations will be made in the subclasses, since they will depend on the detector material.
+
+    def gen_arf(self, energy_lo=None, energy_hi=None, arf="cubesat.arf"):
+        if energy_lo is None or energy_hi is None:
+            energy_edges = np.linspace(self.missions.energymin, self.missions.energymax, self.missions.energymax - self.missions.energymin + 1)
+            energy_lo = energy_edges[:-1]
+            energy_hi = energy_edges[1:]
+
+        energy = 0.5 * (energy_lo + energy_hi)
+        aeff = np.array([self.effective_area(e) for e in energy])
+        
+        #This is the actual ARF Matrix. Col1 is the lower energy bound, Col2 is the upper energy bound, and Col3 is the effective area in cm^2.
+        cols = [
+            fits.Column(name='ENERG_LO', format='D', unit='keV', array=energy_lo),
+            fits.Column(name='ENERG_HI', format='D', unit='keV', array=energy_hi),
+            fits.Column(name='SPECRESP', format='D', unit='cm**2', array=aeff)]
+        
+        arf_hdu = fits.BinTableHDU.from_columns(cols)
+        arf_hdu.name = "SPECRESP"
+
+        #These are the required header keywords for an ARF file, according to the OGIP standard. (https://heasarc.gsfc.nasa.gov/docs/heasarc/caldb/docs/memos/cal_gen_92_002/cal_gen_92_002.html#tth_sEc4)
+        hdr = arf_hdu.header
+        hdr["HDUCLASS"] = "OGIP"
+        hdr["HDUCLAS1"] = "RESPONSE"
+        hdr["HDUCLAS2"] = "SPECRESP"
+        hdr["HDUVERS"]  = "1.1.0"
+        hdr["CHANTYPE"] = "PI"
+        hdr["HDUCLAS3"] = "FULL"
+        hdr["TELESCOP"] = f"{self.missions.name}"
+        hdr["INSTRUME"] = "CZT"
+        hdr["FILTER"]   = "NONE"
+        hdr["EXTNAME"]  = "SPECRESP"
+
+        primary = fits.PrimaryHDU()
+        hdul = fits.HDUList([primary, arf_hdu])
+        hdul.writeto(arf, overwrite=True) #The name of the output ARF file is stored in arf. This will be used to generate the RMF file.
+        return arf
+
+    def gen_rsp(self, arf, rsp="cubesat.rsp", grad=0.0):
+        resolution = self.res #keV FWHM at 1 keV, in eV
+        gradient = grad #eV/keV, the change in resolution with energy
+        subprocess.run(["ogipgenrsp", #This is the xspec command to generate an RMF file from an ARF file. It is part of the HEASoft package.
+            "--arffile", arf, #The arf should be generated first, and then the rmf can be generated from it.
+            "--resolution", str(resolution), 
+            "--resgradient", str(gradient),
+            "--rspfile", rsp, #The name of the output RMF file
+            "--range", f"{self.missions.energymin}:{self.missions.energymax}",
+            "--overwrite"
+        ], check=True)
+
+        return rsp
+
+
+class czt(detector):
+    def __init__(self, geography, orbit, mission):
+        super().__init__(geography, orbit, mission)
+        self.res = 6.63e3
+
+    def effective_area(self, energy):
+        #Energy in kev. xraydb.mu_elam takes energy in eV, so we multiply by 1000 to convert from keV to eV.
+        
+        #soft energy cutoff
+        if energy < 1:
+            return 0
+
+        #mass fractions of Cd, Zn, and Te in CdZnTe
+        w_Cd = 0.425
+        w_Zn = 0.028
+        w_Te = 0.547
+
+        #density of CdZnTe is 5.78 g/cm^3.
+        density = 5.78 #g/cm^3
+        atten_const = (w_Cd * xraydb.mu_elam('Cd', energy*1000) + w_Zn * xraydb.mu_elam('Zn', energy*1000) + w_Te * xraydb.mu_elam('Te', energy*1000))*density
+        return self.geos.collecting_area * (1-np.exp(-atten_const * self.geos.detthickness * 0.1)) #The 0.1 is to convert from mm to cm, since the thickness is in mm and the attenuation constant is in cm^-1.
+
+class silicon (detector):
+    def __init__(self, geography, orbit, mission):
+        super().__init__(geography, orbit, mission)
+        self.res = 120
+
+    def effective_area(self, energy):
+
+        #soft energy cutoff
+        if energy < 1:
+            return 0
+
+        #Energy in keV. xraydb.mu_elam takes energy in eV, so we multiply by 1000 to convert from keV to eV.
+        #density of silicon is 2.33 g/cm^3.
+        density = 2.33 #g/cm^3
+        atten_const = xraydb.mu_elam('Si', energy*1000)*density
+        return self.geos.collecting_area * (1-np.exp(-atten_const * self.geos.detthickness * 0.1)) #The 0.1 is to convert from mm to cm, since the thickness is in mm and the attenuation constant is in cm^-1.
+
---
+++ spectrumTests.py
@@ -0,0 +1,117 @@
+from xspec import *
+import MissionClasses as mc
+import requests
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+#This is for our specific Cubesat
+orb = mc.Orbit(500, 0)
+geo = mc.geometry()
+mission1 = mc.Mission("Cubesat", 15, 150)
+cztDetector = mc.czt(geo, orb, mission1)
+background = mc.CXB(detector=cztDetector)
+exposureTime = 10000 #seconds
+
+AllData.clear()
+AllModels.clear()
+
+print('hello')
+
+'''
+arfname = cztDetector.gen_arf(energy_lo = cztDetector.energy_low, energy_hi = cztDetector.energy_high, arf="cubesat.arf")
+rspname =cztDetector.gen_rsp(arfname)
+backgroundname = background.gen_spectrum_table()
+'''
+
+
+# Run this in the terminal: flx2tab cxb_background.dat cxb cxb.mod
+'''
+m = Model("atable{cxb.mod}")
+
+fake = FakeitSettings(response= 'cubesat.rsp', exposure= exposureTime, fileName="simulated.pha")
+AllData.fakeit(1, fake)
+'''
+
+'''
+AllData("simulated.pha")
+Plot.xAxis = "keV"
+Plot("data")
+energy = Plot.x()
+counts = Plot.y()
+errors = Plot.yErr()
+numcounts = np.array(counts) / exposureTime
+plt.figure(figsize=(8,5))
+plt.step(energy, numcounts, where="mid", color="black")
+plt.errorbar(energy, numcounts, yerr=np.array(errors)/exposureTime, fmt="none", color="black", alpha=0.5)
+plt.xlabel("Energy (keV)")
+plt.ylabel("Count rate (counts/s/bin)")
+plt.yscale("log")
+plt.xscale('log')
+plt.title("Simulated CXB Spectrum")
+plt.savefig("simulated_spectrum.png", dpi=300, bbox_inches="tight")
+plt.close()
+'''
+
+'''
+#playing around and graphing the x-ray background.
+CXB_model = CXB(mission=mission1)
+Albedo_model = Albedo(mission=mission1)
+energies = np.logspace(.5, 2.5, 300)  # Energy range from ~3 keV to ~300 keV
+plt.figure(figsize=(10, 6))
+plt.plot(energies, [geo.fov_sr * CXB_model.photonIntensity(energy) for energy in energies], color='green', label='CXB Background')
+plt.plot(energies, [geo.fov_sr * Albedo_model.photonIntensity(energy) for energy in energies], color='blue', label='Albedo Background')
+plt.plot(energies, [geo.fov_sr * (CXB_model.photonIntensity(energy) + Albedo_model.photonIntensity(energy)) for energy in energies], color='red', label='Total Background')
+plt.xlabel('Energy (keV)')
+plt.ylabel(r"$flux$ ( photons keV$^{-1}$ cm$^{-2}$ s$^{-1}$)")
+plt.title('Background vs Energy')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(1, 1000)
+plt.legend()
+plt.savefig("cxb.png", dpi=300)
+'''
+
+#This is for testing the ARF file from the HEASARC calibration database.
+
+'''
+arf = fits.open("cubesat.arf")
+
+#arf.info() #This is just to check the contents of the ARF file.
+aeff = arf[1].data["SPECRESP"]
+#print(aeff[-5:]) #Check the last 5 values of the effective area.
+
+plt.plot(arf[1].data["ENERG_LO"], aeff, color='purple', marker='o')
+plt.xlabel('Energy (keV)')
+plt.ylabel('Effective Area (cm²)')
+plt.xscale('log')
+plt.title('Effective Area vs Energy for CdZnTe Detector')
+plt.savefig("effective_area.png", dpi=300)
+'''
+
+#This is for testing the effective area of the CdZnTe detector.
+'''
+det = czt(geo, orb, mission1)
+energies = np.linspace(10e3, 500e3, 100)
+area = det.effective_area(energies)
+plt.plot(energies/1e3, area, color='purple', marker='o')
+plt.xlabel('Energy (keV)')
+plt.ylabel('Effective Area (cm²)')
+plt.xscale('log')
+plt.title('Effective Area vs Energy for CdZnTe Detector')
+plt.savefig("effective_area.png", dpi=300)
+'''
+
+#This is for testing the RMF file from the HEASARC calibration database.
+'''
+url = "https://swift.gsfc.nasa.gov/proposals/bat.rsp"
+r = requests.get(url)
+r.raise_for_status()
+with open("bat.rsp", "wb") as f:
+    f.write(r.content)
+rmf = fits.open("bat.rsp")
+matrix = rmf["EBOUNDS"].data
+print(matrix[:5])
+'''

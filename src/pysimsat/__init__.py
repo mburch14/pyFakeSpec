3e1f450ca545c05e0b7d05c77e388a614b80cb8e
Michael Burch <michaelburch797@gmail.com>
2026-08-05 16:18:25 -0700

    v0.1.1

--- src/pysimsat/__init__.py
+++ src/pysimsat/__init__.py
@@ -3,7 +3,7 @@ from .genBKG import gen_background
 from .genOBS import gen_observation
 from .workflow import run_all
 
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 __all__ = ["gen_rsp", "gen_background", "gen_observation", "run_all"]
 
